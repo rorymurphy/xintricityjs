7c483350ed4fb1a577c0e48478cafd05f65a22e0
Rory <ndmurph04@gmail.com>
2013-12-16 00:07:46 -0500

    Updated readme file Added a bunch of test data Slightly updated sample

--- tests/xin-tests-data.js
+++ tests/xin-tests-data.js
@@ -2,56 +2,172 @@ var musicLibrary = [
     {
         Title: "A Hard Day's Night",
         Artist: "The Beatles",
+        Year: 1964,
         Slug: 'a-hard-days-night',
         IsFavorite: false,
         Songs: [
             {
                 Title: "A Hard Day's Night",
-                Artist: "The Beatles"
+                Artist: "Lennon and McCartney"
             },
             {
-                Title: "If I Fell in Love",
-                Artist: "Paul McCartney",
+                Title: "I Should Have Known Better",
+                Artist: "Lennon"
+            },
+            {
+                Title: "If I Fell",
+                Artist: "Lennon and McCartney"
+            },
+            {
+                Title: "I'm Happy Just to Dance with You",
+                Artist: "Harrison"
+            },
+            {
+                Title: "And I Love Her",
+                Artist: "McCartney"
+            },
+            {
+                Title: "Tell Me Why",
+                Artist: "Lennon with McCartney"
             },
             {
                 Title: "Can't Buy Me Love",
-                Artist: "John Lennon"
+                Artist: "McCartney"
+            },
+            {
+                Title: "Any Time at All",
+                Artist: "Lennon"
+            },
+            {
+                Title: "I'll Cry Instead",
+                Artist: "Lennon"
+            },
+            {
+                Title: "Things We Said Today",
+                Artist: "McCartney"
+            },
+            {
+                Title: "When I Get Home",
+                Artist: "Lennon"
+            },
+            {
+                Title: "You Can't Do That",
+                Artist: "Lennon"
+            },
+            {
+                Title: "I'll Be Back",
+                Artist: "Lennon with McCartney"
             }
+            
         ]
     },
     {
         Title: "Superunknown",
-        Artist: "The Beatles",
+        Artist: "Soundgarden",
+        Year: 1994,
         Slug: 'superunknown',
-        IsFavorite: true,
+        IsFavorite: false,
         Songs: [
             {
-                Title: "Spoon Man",
-                Artist: "Sound Garden"
+                Title: "Let Me Down",
+                Artist: "Soundgarden"
+            },
+            {
+                Title: "My Wave",
+                Artist: "Cornell, Kim Thayil"
+            },
+            {
+                Title: "Fell on Black Days",
+                Artist: "Soundgarden"
+            },
+            {
+                Title: "Mailman",
+                Artist: "Matt Cameron"
+            },
+            {
+                Title: "Superunknown",
+                Artist: "Cornell, Thayil"
+            },
+            {
+                Title: "Head Down",
+                Artist: "Shepherd"
             },
             {
                 Title: "Black Hole Sun",
-                Artist: "Sound Garden"
+                Artist: "Soundgarden"
+            },         
+            {
+                Title: "Spoonman",
+                Artist: "Soundgarden"
+            },
+            {
+                Title: "Limo Wreck",
+                Artist: "Cameron, Thayil"
+            },
+            {
+                Title: "The Day I Tried to Live",
+                Artist: "Soundgarden"
+            },
+            {
+                Title: "Kickstand",
+                Artist: "Thayil"
+            },
+            {
+                Title: "Fresh Tendrils",
+                Artist: "Cornell, Cameron"
+            },
+            {
+                Title: "4th of July",
+                Artist: "Soundgarden"
+            },
+            {
+                Title: "Half",
+                Artist: "Shepherd"
+            },
+            {
+                Title: "Like Suicide",
+                Artist: "Shepherd"
             }
         ]
     },
     {
-        Title: "Big Ones",
-        Artist: "Aerosmith",
-        Slug: "big-ones",
-        IsFavorite: false,
+        Title: "Brothers in Arms",
+        Artist: "Dire Straits",
+        Year: 1985,
+        Slug: "brothers-in-arms",
+        IsFavorite: true,
         Songs: [
             {
-                Title: "Walk on Water",
-                Artist: "Aerosmith"
+                Title: "So Far Away",
+                Artist: "Dire Straits"
             },
             {
-                Title: "Love in an Elevator",
-                Artist: "Aerosmith"
+                Title: "Money For Nothing",
+                Artist: "Dire Straits with Sting"
             },
             {
-                Title: "Rag Doll",
-                Artist: "Aerosmith"
+                Title: "Your Latest Trick",
+                Artist: "Dire Straits"
+            },
+            {
+                Title: "Why Worry",
+                Artist: "Dire Straits"
+            },
+            {
+                Title: "Ride Across the River",
+                Artist: "Dire Straits"
+            },
+            {
+                Title: "The Man's Too Strong",
+                Artist: "Dire Straits"
+            },
+            {
+                Title: "One World",
+                Artist: "Dire Straits"
+            },
+            {
+                Title: "Brothers in Arms",
+                Artist: "Dire Straits"
             }
         ]
     }
